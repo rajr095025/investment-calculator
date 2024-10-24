43d522d5ba836b771983faa1d67a4c53a0d47a60
Rakesh Raj B S <rajr095025@gmail.com>
2024-10-24 23:00:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import HeaderLogo from "./assets/investment-calculator-logo.png"
+
+
+function App() {
+  return (
+    // <h1>React Investment Calculator</h1>
+    <>
+      <div id ="header">
+        <img src={HeaderLogo}></img>
+        <div>
+        <div id="user-input">
+          
+          </div>
+        </div>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default App
